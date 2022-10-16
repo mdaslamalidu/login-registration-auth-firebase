4b2b3fd77a43ebca773162e7957d8bf2334ee694
Md Aslam Ali <mdaslamali675@gmail.com>
2022-10-16 07:17:55 +0600

    add authetication

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD6Mohs9nsujjEidK4dITXiJkgqegCV24s",
+    authDomain: "auth-router-context-e522f.firebaseapp.com",
+    projectId: "auth-router-context-e522f",
+    storageBucket: "auth-router-context-e522f.appspot.com",
+    messagingSenderId: "478819992241",
+    appId: "1:478819992241:web:a56071f5fe20c64d6ed890"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
